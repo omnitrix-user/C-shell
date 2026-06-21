243d6e843163bdb17ccb0e49b84107dd8fcf1804
Ashish <abhishek@qualtechedge.com>
2026-06-21 15:49:46 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,7 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ invalid_banana_command";
+  std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
   std::cout << input << ": command not found" << std::endl;
